7bedb07662e4045c7b78cb44539f0c354cbb4e85
HamzaSafeer12 <hamzasafeer243@gmail.com>
2024-10-25 22:31:49 -0700

    update Inquiry model buyer field null true or blank true

--- RealEstateApp/models/Inquiry.py
+++ RealEstateApp/models/Inquiry.py
@@ -3,9 +3,9 @@ from RealEstateApp.models import User
 from RealEstateApp.models import Property
 
 class Inquiry(models.Model):
-    buyer = models.ForeignKey(User, on_delete=models.CASCADE)
+    buyer = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     property = models.ForeignKey(Property, on_delete=models.CASCADE)
     message = models.TextField()
-    status = models.CharField(max_length=20, choices=[('Pending', 'Pending'), ('Approved', 'Approved'), ('Declined', 'Declined')])
+    status = models.CharField(max_length=20,default='Pending', choices=[('Pending', 'Pending'), ('Approved', 'Approved'), ('Declined', 'Declined')])
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
