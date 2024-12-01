f266901a57a01092d15834498d7747fc6676b9b8
HamzaSafeer12 <hamzasafeer243@gmail.com>
2024-12-01 16:43:48 +0000

    Configure Swagger in project urls.py

--- RealEstate/urls.py
+++ RealEstate/urls.py
@@ -16,14 +16,47 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+# Create the schema view
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Your API Title",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@yourapi.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
+# Define your URL patterns here
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('RealEstateApp.urls')),
+    path('api/', include('RealEstateApp.urls')),  # existing app URLs
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
 ]
+
+# Now, let's set the security schemes in the schema after it's been created
+schema_view._schema = {
+    'components': {
+        'securitySchemes': {
+            'Bearer': {
+                'type': 'http',
+                'scheme': 'bearer',
+                'bearerFormat': 'JWT',
+            },
+        },
+    },
+}
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
